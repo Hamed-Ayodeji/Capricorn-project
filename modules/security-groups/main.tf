9a73edb026f3bf775ef9c4f5f19ce4f9d045879f
Ayodeji Hamed <ayodejihamed@gmail.com>
2024-01-03 17:27:58 +0100

    Update main.tf

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -1,4 +1,4 @@
-# create security group for the public subnet
+# create security group for the public instance
 resource "aws_security_group" "public-instance-sg" {
   name               = "${var.project_name}-public-instance-sg"
   description        = "enable http/https access on port 80/443, and ssh access on 22"
@@ -40,7 +40,7 @@ resource "aws_security_group" "public-instance-sg" {
   }
 }
 
-# create security group for the private subnet
+# create security group for the private instance
 resource "aws_security_group" "private-instance-sg" {
   name               = "${var.project_name}-private-instance-sg"
   description        = "allow ssh and icmp access from the public instance security group"
